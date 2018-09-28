cc53937afeaed6138238b52227e27967b6b244d1
amlingc5 <35497814+amlingc5@users.noreply.github.com>
2018-09-28 19:53:10 -0400

    Update MSP430G2.c

--- Button Based Delay/MSP430G2.c
+++ Button Based Delay/MSP430G2.c
@@ -1,50 +1,48 @@
 #include <msp430.h>
 
-volatile unsigned int TC1 = 0;  // Timer Counter 1 initialized to 0 (Counter for P1.0)
+volatile unsigned int TC1 = 0;  // Timer set initialized to 0
 
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;   // Disable the watchdog timer
-	BCSCTL3 = LFXT1S_2;         // Makes ACLK use internal oscillator
+	BCSCTL3 = LFXT1S_2;         // Enables internal oscillator
 
-	P1DIR |= BIT6;  // Set P1.6 (Red LED) to output
-    P1REN |= BIT3;   // Enable internal resistor
-    P1OUT |= BIT3;   // Connects internal resistor to power (making it a pull-up)
+	P1DIR |= BIT6;  // Set P1.6 to output
+    	P1REN |= BIT3;   // Enable internal resistor
+    	P1OUT |= BIT3;   // Enable Pull-up Resistor
 
-    P1IE |= BIT3;    // Set P1.3 as interrupt pin (interrupts are triggered w/ changes to this pin)
-    P1IES |= BIT3;   // Set P1.3 to interrupt on falling edge (high to low transition)
+    	P1IE |= BIT3;    // Set P1.3 as interrupt pin
+    	P1IES |= BIT3;   // Set P1.3 to interrupt on falling edge
 
-    TACCTL0 = CCIE; // Capture Compare Interrupt Enabled
-    TACCR0 = 100;   // Set Capture Compare Register
-    TACTL = TASSEL_1 + MC_1 + ID_3; // Timer A Control: ACLK, Up Mode, Internal Divider (Divide ACLK by 8)
+ 	TACCTL0 = CCIE; // Capture Compare Interrupt Enabled
+  	TACCR0 = 100;   // Set Register
+  	TACTL = TASSEL_1 + MC_1 + ID_3; // Timer A Control: ACLK, Up Mode, Internal Divider
 
 
     __enable_interrupt();   // Enables interrupt algorithm
-    for(;;){};  // Infinite for loop
+    for(;;){};  //Continuous loop
 }
 
-// Interrupt service routine for TimerA0
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A(void)
 {
-    P1OUT ^= BIT6;  // Toggle P1.6 (Red LED ON)
+    P1OUT ^= BIT6;  // Toggle P1.6
 }
 
-// Interrupt service routine for Port 1
 #pragma vector = PORT1_VECTOR
 __interrupt void Port_1(void)
 {
-    if(P1IES & BIT3){   // Port 1 Interrupt Edge Select (Falling Edge)
+    if(P1IES & BIT3){   //Interrupt falling Edge
 
-        TACTL = TACLR;  // Clear Timer Settings
-        TACTL = TASSEL_1 + MC_2 + ID_3; // Timer A Control: ACLK, Continuous mode, Internal Divider (Divide ACLK by 8)
-        P1IES &= ~BIT3; // Set Interrupt Edge Select (Rising Edge)
+        TACTL = TACLR;  // Clear Timer
+        TACTL = TASSEL_1 + MC_2 + ID_3; // Timer A Control: ACLK, Continuous mode, Internal Divider
+        P1IES &= ~BIT3; // Set Rising EdgeI nterrupt
     }
 
     else{
-        TACCR0 = TA0R;  // Sets Capture Compare register to TimerA0 value
-        TACTL = TASSEL_1 + MC_1 +ID_3;  // Timer A Control: ACLK, Up mode, Internal Divider (Divide ACLK by 8)
-        P1IES |= BIT3;  // Set Interrupt Edge Select (Falling Edge)
+        TACCR0 = TA0R;  // Sets register
+        TACTL = TASSEL_1 + MC_1 +ID_3;  // Timer A Control: ACLK, Up mode, Internal Divider
+        P1IES |= BIT3;  // Set Falling Edge Interrupt 
     }
 
     P1IFG &= ~BIT3;    // Resets interrupt flag for P1.3
